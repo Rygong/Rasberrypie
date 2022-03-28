6076a0bd349471c3394f466e9626d142eeaec775
Rygong <mecca0504@naver.com>
2022-03-28 15:51:18 +0900

    220328

---
+++ python/baekjoon2588_failure.py
@@ -0,0 +1,17 @@
+A, B, C = input().split()
+D, E, F = input().split()
+
+SUM = A+B+C
+SUM_2 = D+E+F
+
+SUM = int(SUM)
+SUM_2 = int(SUM_2)
+
+D = int(D)
+E = int(E)
+F = int(F)
+
+print(SUM*F)
+print(SUM*E)
+print(SUM*D)
+print(SUM*SUM_2)
